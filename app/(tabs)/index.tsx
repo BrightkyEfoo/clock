6e0ea779515d29757d68b3db9959748576be8755
Bright Efoo <brightefoo@gmail.com>
2024-12-17 09:42:29 +0100

    clock app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,245 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { transform } from "@babel/core";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedProps,
+  useDerivedValue,
+  useSharedValue,
+  withDecay,
+  withDelay,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+import Svg, {
+  Circle,
+  Defs,
+  FeGaussianBlur,
+  Filter,
+  Line,
+  Path,
+  Text as SvgText,
+} from "react-native-svg";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const { width, height } = Dimensions.get("screen");
+
+const _clockRadius = width * 0.3;
+
+const _indicatorBoxSize = 30;
+
+// differentiels de translation de repere au centre
+const _dx = width / 2;
+const _dy = width / 2;
+
+const _indicatorSpaceToClock = 2;
+
+const _smallIndicatorsSnapAngle = 30; // en degrees
+const _smallIndicatorsBigRadius = _clockRadius * 0.92; // rayon du cercle formé par les pointes les plus proches du cercle des indicateurs
+const _smallIndicatorsSmallRadius = _clockRadius * 0.84; // rayon du cercle formé par les pointes les plus eliognees du cercle des indicateurs
+
+const _secondHandSize = _clockRadius;
+const _minuteHandSize = _clockRadius * 1.2;
+const _hourHandSize = _clockRadius * 1.4;
+
+const degToRad = (degrees: number) => (degrees * Math.PI) / 180;
+
+const sin = Math.sin;
+const cos = Math.cos;
+
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 export default function HomeScreen() {
+  const secondHandAngle = useSharedValue(0);
+
+  const secondHandRotate = useAnimatedProps(() => {
+    return {
+      transform: `rotate(${98} ${width / 2},${width / 2})`,
+      // rotation: secondHandAngle.value,
+    };
+  }, [secondHandAngle]);
+
+  // useEffect(() => {
+  //   secondHandAngle.value = withRepeat(
+  //     withTiming(, { duration: 60000, easing: (t) => t }), // 60 secondes pour 360 degrés
+  //     Infinity, // répéter indéfiniment
+  //     true // sans inversion
+  //   );
+  // }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#040316",
+      }}
+    >
+      <StatusBar hidden />
+
+      <View
+        style={{
+          height: width,
+          width: width,
+          position: "relative",
+          borderWidth: 1,
+          borderColor: "white",
+        }}
+      >
+        <Svg height={width} width={width}>
+          <Defs>
+            <Filter id="shadow">
+              <FeGaussianBlur stdDeviation="100" />
+            </Filter>
+          </Defs>
+          {/* Ombre floutée */}
+          <Circle
+            cx={"50%"}
+            cy={"50%"}
+            r={_clockRadius + 2}
+            fill="none"
+            stroke="#dbd66eff"
+            strokeOpacity={0.4}
+            filter="url(#shadow)"
+          />
+          {/* Cercle principal */}
+          <Circle
+            cx={"50%"}
+            cy={"50%"}
+            r={_clockRadius}
+            fill="none"
+            stroke={"#dbd66e3a"}
+          />
+
+          {/* hour hand  */}
+          <AnimatedPath
+            stroke={"#008a29ff"}
+            d={`M ${width / 2},${width / 2} V ${_hourHandSize}`}
+            strokeWidth={5}
+            originX={width / 2}
+            originY={width / 2}
+          />
+
+          {/* second hand  */}
+          <AnimatedPath
+            stroke={"#155c00ff"}
+            d={`M ${width / 2},${width / 2 + 20} V ${_secondHandSize}`}
+            strokeWidth={1}
+            animatedProps={secondHandRotate}
+            originX={width / 2}
+            originY={width / 2}
+          />
+
+          <Circle cx={"50%"} cy={"50%"} r={5} fill="#8b8b8bff" />
+
+          {/* minute hand  */}
+          <AnimatedPath
+            stroke={"#8b8b8bff"}
+            d={`M ${width / 2},${width / 2} V ${_minuteHandSize}`}
+            strokeWidth={3}
+          />
+
+          {Array(12)
+            .fill(0)
+            .map((_, idx) => {
+              if (idx % 3 === 0) return null;
+              const angle = degToRad(idx * _smallIndicatorsSnapAngle);
+              const x1 = _smallIndicatorsBigRadius * cos(angle) + _dx;
+              const y1 = _smallIndicatorsBigRadius * sin(angle) + _dy;
+
+              const x2 = _smallIndicatorsSmallRadius * cos(angle) + _dx;
+              const y2 = _smallIndicatorsSmallRadius * sin(angle) + _dy;
+
+              return (
+                <Line
+                  key={`small-indicator-${idx}`}
+                  x1={x1}
+                  y1={y1}
+                  x2={x2}
+                  y2={y2}
+                  stroke={"#d6d1703a"}
+                />
+              );
             })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        </Svg>
+
+        <View
+          style={[
+            styles.indicatorBox,
+            {
+              top: -_clockRadius + _dy,
+              left: _dx,
+              transform: [
+                { translateX: -_indicatorBoxSize / 2 },
+                { translateY: _indicatorSpaceToClock },
+              ],
+            },
+          ]}
+        >
+          <Text style={[styles.indicatorText]}>12</Text>
+        </View>
+        <View
+          style={[
+            styles.indicatorBox,
+            {
+              top: _dy,
+              left: _clockRadius + _dx,
+              transform: [
+                { translateY: -_indicatorBoxSize / 2 },
+                { translateX: -_indicatorSpaceToClock - _indicatorBoxSize },
+              ],
+            },
+          ]}
+        >
+          <Text style={[styles.indicatorText]}>3</Text>
+        </View>
+
+        <View
+          style={[
+            styles.indicatorBox,
+            {
+              top: _clockRadius + _dy,
+              left: _dx,
+              transform: [
+                { translateX: -_indicatorBoxSize / 2 },
+                { translateY: -_indicatorSpaceToClock - _indicatorBoxSize },
+              ],
+            },
+          ]}
+        >
+          <Text style={[styles.indicatorText]}>6</Text>
+        </View>
+      </View>
+
+      <View
+        style={[
+          styles.indicatorBox,
+          {
+            top: _dy,
+            left: -_clockRadius + _dx,
+            transform: [
+              { translateY: -_indicatorBoxSize / 2 },
+              { translateX: _indicatorSpaceToClock },
+            ],
+          },
+        ]}
+      >
+        <Text style={[styles.indicatorText]}>9</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  indicatorBox: {
+    width: _indicatorBoxSize,
+    height: _indicatorBoxSize,
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  indicatorText: {
+    color: "#d6d1703a",
+    fontSize: 14,
+    fontWeight: "bold",
   },
 });
